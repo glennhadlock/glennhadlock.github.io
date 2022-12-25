ca66668f3434bf9ca4d5751b78aba81c14b600f0
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2022-12-25 17:40:06 -0500

    added map

---
+++ src/components/GMap.js
@@ -0,0 +1,9 @@
+export default function GMap() {
+  return (
+    <iframe
+      className="gmap"
+      id="gmap_canvas"
+      src="https://maps.google.com/maps?width=834&amp;height=411&amp;hl=en&amp;q=5664%20Bee%20Ridge%20Road,%20Suite%20102%20Sarasota+(Glenn%20Hadlock,%20DC)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+    ></iframe>
+  );
+}
--- src/views/ContactView/ContactView.js
+++ src/views/ContactView/ContactView.js
@@ -1,17 +1,32 @@
 import GenericView from "../GenericView/GenericView";
+import GMap from "../../components/GMap";
 export default function ContactView() {
   return (
     <GenericView title="contact us" banner="https://images.pexels.com/photos/4506078/pexels-photo-4506078.jpeg">
       <p>Glenn A. Hadlock, D.C. - Chriopractic Physician</p>
-      <p>
-        Telephone: <b>941-925-1517</b>
-      </p>
-      <p>
-        Address: <b>5664 Bee Ridge Road, Suite 102 Sarasota, FL 34233</b>
-      </p>
-      <p>
-        Information: <b>Please Contact us for appointments, questions or any other concerns. Looking forward to helping you live "Pain Free"!</b>
-      </p>
+      <table>
+        <tbody>
+          <tr>
+            <td>Telephone</td>
+            <td>
+              <b>941-925-1517</b>
+            </td>
+          </tr>
+          <tr>
+            <td>Address</td>
+            <td>
+              <b>5664 Bee Ridge Road, Suite 102 Sarasota, FL 34233</b>
+            </td>
+          </tr>
+          <tr>
+            <td>Information</td>
+            <td>
+              <b>Please Contact us for appointments, questions or any other concerns. Looking forward to helping you live "Pain Free"!</b>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <GMap />
     </GenericView>
   );
 }
